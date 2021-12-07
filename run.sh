dedce07bc397c455891f0f324f92d33ddc2819d8
Kenneth <kenneth@ubuntuserv05.home>
2021-12-07 23:11:27 +0100

    fetch, pull.. yeah, i see the difference :)

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git fetch
+git pull
 docker build -t bells_and_whistles:1.0 .
 docker run --rm -v $(pwd)/build/bells_and_whistles_to_slack.yml:/ansible/bells_and_whistles_to_slack.yml --network webproxy bells_and_whistles:1.0
