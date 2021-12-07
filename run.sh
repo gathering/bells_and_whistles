3269473340d6e2eea7971c550d89494183e3c933
Kenneth <kenneth@ubuntuserv05.home>
2021-12-07 23:08:46 +0100

    git fetch could be helpful in run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+git fetch
 docker build -t bells_and_whistles:1.0 .
 docker run --rm -v $(pwd)/build/bells_and_whistles_to_slack.yml:/ansible/bells_and_whistles_to_slack.yml --network webproxy bells_and_whistles:1.0
